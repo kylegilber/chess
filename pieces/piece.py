2525e1f8e0d2257b202be5b6ace46f46473582f9
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-06-09 17:08:45 -0400

    Update piece.py
    Add function to convert square position on board (single number corresponding to a square) to coordinates.

--- pieces/piece.py
+++ pieces/piece.py
@@ -1,4 +1,12 @@
+import math
+
 class Piece:
 
-    def __init__(self):
-        pass
+    def __init__(self, assoc = None, pos = None):
+        self.association = assoc
+        self.position = pos
+
+    def getCoords(self):
+        a=self.position/8
+        b=self.position%8
+        return[math.floor(a),b]
