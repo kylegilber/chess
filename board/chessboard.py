7fed7dfb20dc659da40da08180653f834bc4b2ae
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-06-13 15:05:29 -0400

    Update chessboard.py
    Create a function to set up the initial state of the chess board.

--- board/chessboard.py
+++ board/chessboard.py
@@ -11,4 +11,16 @@ class Board:
 
     gamestate = [[0 for rank in range(8)] for file in range(8)]
 
-    gamestate
+    def setupBoard(self):
+
+        order = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+
+        for file in range(8):
+            self.gamestate[0][file] = Square(file, order[file]("Black", file))
+            self.gamestate[1][file] = Square(file + 8, Pawn("Black", file + 8))
+            self.gamestate[6][file] = Square(file + 48, Pawn("White", file + 48))
+            self.gamestate[7][file] = Square(file + 56, order[file]("White", file + 56))
+
+        for rank in range(2, 6):
+            for file in range(8):
+                self.gamestate[rank][file] = Square(rank * 8 + file)
