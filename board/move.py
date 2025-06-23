9f59a54b9af3303be8c35f305cd97b5b22e52d29
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-06-23 15:49:49 -0400

    Update move.py
    Define the enpassant function to check whether a pawn is capable of taking another pawn en passant.

--- board/move.py
+++ board/move.py
@@ -9,3 +9,35 @@ class Move:
         pos = rank * 8 + file
         return pos
 
+    def enpassant(self, gamestate, rank, file):
+        """
+        """
+
+        moves = []
+        cur = gamestate[rank][file].piece
+
+        if (rank == 4):
+            if (cur.tostring() == "p"):
+                if (file + 1 < 8):
+                    temp = gamestate[rank][file+1].piece
+                    if (temp.tostring() == "P" and temp.enpassant == True):
+                        moves.append([rank, file+1])
+                if (file - 1 > -1):
+                    temp = gamestate[rank][file-1].piece
+                    if (temp.tostring() == "P" and temp.enpassant == True):
+                        moves.append([rank, file-1])
+        elif (rank == 3):
+            if (cur.tostring() == "P"):
+                if (file + 1 < 8):
+                    temp = gamestate[rank][file+1].piece
+                    if (temp.tostring() == "p" and temp.enpassant == True):
+                        moves.append([rank, file+1])
+                if (file - 1 > -1):
+                    temp = gamestate[rank][file-1].piece
+                    if (temp.tostring() == "p" and temp.enpassant == True):
+                        moves.append([rank, file-1])
+
+        return moves
+
+    
+
