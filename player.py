d5f633273dfe5184e06bbe9eb3a32790d4d799c9
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-01 22:02:34 -0400

    Update player.py
    Add color constants, main loop, and define the renderSquare function which draws the squares that constitute the chess board.

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 import pygame
 from board.chessboard import Board
+from board.move import Move
 
 # Initialize pygame
 pygame.init()
 
-# Set display size
-gameDisplay = pygame.display.set_mode((800, 800))
+# Set window dimensions
+window = pygame.display.set_mode((800, 800))
 
 # Caption
 pygame.display.set_caption("Chess")
@@ -18,3 +19,23 @@ board = Board()
 board.setupBoard()
 board.printBoard()
 
+# Colors
+dark = (119, 149, 86)
+light = (235, 236, 208)
+
+playing = True
+while playing:
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            playing = False
+            pygame.quit()
+
+def renderSquare(color, w, h, x, y):
+    """
+    """
+
+    pygame.draw.rect(
+        surface= window,
+        color= color,
+        rect= [x, y, w, h]
+    )
