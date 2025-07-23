ec4f8dda935a7d8e9e5dc973a3e6fc0c31ef445b
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-23 16:23:35 -0400

    Update chessboard.py
    Setup the initial game state by hardcoding the hexadecimal values representing the starting positions for each piece type.

--- board/chessboard.py
+++ board/chessboard.py
@@ -4,15 +4,20 @@ class Board:
 
     def __init__(self):
         """
-        Initialize 64-bit bitboards representing chessboard
-        squares for each piece type and color.
+        Initialize 64-bit bitboards representative of 
+        chessboard squares for each piece type and color.
         """
 
-        self.whiteBitBoard = np.uint64(0) 
-        self.blackBitBoard = np.uint64(0) 
-        self.bishopBitBoard = np.uint64(0)
-        self.kingBitBoard = np.uint64(0) 
-        self.knightBitBoard = np.uint64(0) 
-        self.pawnBitBoard = np.uint64(0) 
-        self.queenBitBoard = np.uint64(0) 
-        self.rookBitBoard = np.uint64(0)
+        # Pieces
+        self.bishop = np.uint64(0x2400000000000024)
+        self.king = np.uint64(0x1000000000000010) 
+        self.knight = np.uint64(0x4200000000000042) 
+        self.pawn = np.uint64(0x00FF00000000FF00) 
+        self.queen = np.uint64(0x0800000000000008) 
+        self.rook = np.uint64(0x8100000000000081)
+
+        # Colors
+        self.white = np.uint64(0x000000000000FFFF) 
+        self.black = np.uint64(0xFFFF000000000000) 
+
+    
