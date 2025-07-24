5b089e0b9ed72672c1f13034cb7b7c5c57a45b5e
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-23 23:19:09 -0400

    Update piece.py
    Add getIndex function to enable all subclasses to convert coordinates to the corresponding square's index.

--- pieces/piece.py
+++ pieces/piece.py
@@ -17,3 +17,16 @@ class Piece:
         rank = index // 8
         file = index % 8
         return rank, file
+    
+    def getIndex(self, rank, file):
+        """
+        Calculate square index from coordinates.
+
+        {args}
+        rank (int): row of square
+        file (int): column of square
+
+        {returns} index corresponding to square.
+        """
+
+        return rank * 8 + file
