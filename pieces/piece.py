2b7c2b7f943566d1eb1a3b8195679edbf77bce45
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-24 14:16:15 -0400

    Update piece.py
    Define maskAttacks function to reduce repeated code inside subclasses.

--- pieces/piece.py
+++ pieces/piece.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Piece:
 
     def __init__(self):
@@ -30,3 +32,34 @@ class Piece:
         """
 
         return rank * 8 + file
+
+    def maskAttacks(self, square, moves):
+        """
+        Generate attack bitboards for color-independent 
+        pieces from a list of their possible movements.
+
+        {args}
+        square (int): index of square
+        moves (list): rank, file tuples
+
+        {returns}
+        Bitboard with attack squares set to 1
+        """
+
+        # Init empty bitboard
+        mask = np.uint64(0)
+
+        rank, file = self.getCoord(square)
+        for xrank, xfile in moves:
+            r = rank + xrank
+            f = file + xfile
+
+            # Accumulate valid attacks
+            if (-1 < r < 8 and -1 < f < 8):
+                index = self.getIndex(r, f)
+                mask |= np.uint64(1) << index
+
+        return mask
+
+
+
