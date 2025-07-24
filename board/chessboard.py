1900df4ab4c5cec8047475606a6e2ca7c99ddd8f
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-23 22:47:40 -0400

    Update chessboard.py
    Update documentation.

--- board/chessboard.py
+++ board/chessboard.py
@@ -12,7 +12,7 @@ class Board:
         self.bishop = np.uint64(0x2400000000000024)
         self.king = np.uint64(0x1000000000000010) 
         self.knight = np.uint64(0x4200000000000042) 
-        self.pawn = np.uint64(0x00FF00000000FF00) 
+        self.pawn = np.uint64(0x00FF00000000FF00)
         self.queen = np.uint64(0x0800000000000008) 
         self.rook = np.uint64(0x8100000000000081)
 
@@ -24,10 +24,11 @@ class Board:
         """
         Calculate square index from coordinates.
 
-        :arg rank: row of square
-        :arg file: column of square
+        {args}
+        rank (int): row of square
+        file (int): column of square
 
-        :returns: index corresponding to square.
+        {returns} index corresponding to square.
         """
 
         return rank * 8 + file
@@ -36,11 +37,12 @@ class Board:
         """
         Get the bit state at an index.
 
-        :args:
-        bitboard (uint64) - bitboard to check
-        index (int) - index of square
+        {args}
+        bitboard (uint64): bitboard to check
+        index (int): index of square
 
-        :returns: Bit state at index corresponding to a square.
+        {returns} 
+        Bit state at index corresponding to a square.
         """
 
         return 1 if int(bitboard) & (1 << index) else 0
