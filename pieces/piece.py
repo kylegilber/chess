6efbf0fc461d5ef9af905a4b2e6cae24f514a8bb
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-23 23:16:22 -0400

    Update piece.py
    Implement getCoord function to allow all subclasses of Piece to convert indices to coordinates.

--- pieces/piece.py
+++ pieces/piece.py
@@ -3,4 +3,17 @@ class Piece:
     def __init__(self):
         self.attacks = [0 for square in range(64)]
 
-    
+    def getCoord(self, index):
+        """
+        Convert square index to coordinates.
+
+        {args}
+        index (int): index corresp to square
+
+        {returns}
+        The square's rank & file coordinates.
+        """
+
+        rank = index // 8
+        file = index % 8
+        return rank, file
