a66a47b5574a7b88bc4de75abbfc65af0122ec71
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-24 14:27:47 -0400

    Update king.py
    Define the getAttacks function for Kings.

--- pieces/king.py
+++ pieces/king.py
@@ -3,9 +3,22 @@ from pieces.piece import Piece
 class King(Piece):
 
     MOVES = [
-        (1,0), (0,1), (1,1), (1,-1), 
-        (-1,1),(-1,-1),(-1,0),(0,-1)
+        (1, 0), (0, 1), (1, -1), (-1, -1),
+        (-1, 1), (1, 1), (-1, 0), (0, -1)
     ]
 
     def __init__(self):
         super().__init__()
+
+    def getAttacks(self):
+        """
+        Generate attack tables for Kings.
+
+        For each possible knight position on the board, update 
+        self.attacks to store which squares the king can attack
+        at that position.
+        """
+
+        for square in range(64):
+            mask = self.maskAttacks(square, self.MOVES)
+            self.attacks[square] = mask
