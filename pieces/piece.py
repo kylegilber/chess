fe598f3b63dd8d2d17f1bb8e63d5ab1a3929288f
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-23 20:35:04 -0400

    Update piece.py
    Update superclass constructor and define class variables.

--- pieces/piece.py
+++ pieces/piece.py
@@ -1,3 +1,9 @@
 class Piece:
 
-    def __init__(self): pass
+    WHITE = 0
+    BLACK = 1
+
+    def __init__(self):
+        self.moves = [[0 for square in range(64)] for color in range(2)]
+
+    
