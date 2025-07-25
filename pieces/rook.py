25b29c3c17c97e220dbf42582ed62074d3efaa91
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-24 22:00:57 -0400

    Add rook attack table generation with magic bitboards
    Introduces a getAttacks method to the Rook class that generates attack tables using magic bitboards. This prepares efficient move generation by precomputing attack patterns for all board positions and blocker permutations.

--- pieces/rook.py
+++ pieces/rook.py
@@ -1,4 +1,5 @@
 from pieces.piece import Piece
+import magicmoves
 
 class Rook(Piece):
 
@@ -7,4 +8,29 @@ class Rook(Piece):
     ]
 
     def __init__(self):
-        pass
+        self.attacks = [{} for square in range(64)]
+
+    def getAttacks(self):
+        """
+        Generate attack tables for Rooks.
+
+        For each possible bishop position on the board,
+        update the attacks instance variable to store which
+        squares the Rook can attack at that position.
+        """
+
+        for square in range(64):
+
+            # Generate blockers
+            mask = self.maskBlockers(square, self.DIRECTIONS)
+
+            # Generate all blocker permutations
+            permutations = self.permuteBlockers(mask)
+
+            magicNum = magicmoves.rookMagic
+            shiftNum = magicmoves.rookShift
+
+            for blocker in permutations:
+                index = ((blocker & mask) * magicNum) >> shiftNum
+                squares = self.maskSlidingAttacks(square, self.DIRECTIONS, blocker)
+                self.attacks[square][index] = squares
