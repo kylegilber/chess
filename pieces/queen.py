0febed2af40e73656de570e419fd86de7563b4bb
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-24 23:09:47 -0400

    Update queen.py
    Introduces a getAttacks method to the Queen class, which generates attack tables using magic bitboards. This prepares efficient move generation by precomputing attack patterns for all board positions and blocker permutations.

--- pieces/queen.py
+++ pieces/queen.py
@@ -1,7 +1,56 @@
 from pieces.piece import Piece
+import magicmoves
 
 class Queen(Piece):
 
-    directions = [(1,0),(1,1),(0,1),(0,-1),(-1,-1),(-1,0),(-1,1),(1,-1)]
+    bDIRECTIONS = [
+        (1, 1), (-1, 1), (1, -1), (-1, -1)
+    ]
 
-    def __init__(self): pass
+    rDIRECTIONS = [
+        (-1, 0), (1, 0), (0, 1), (0, -1)
+    ]
+
+    def __init__(self):
+        self.attacks = [{} for square in range(64)]
+
+    def getAttacks(self):
+        """
+        Generate attack tables for Queens.
+
+        For each possible Queen position on the board,
+        update the attacks instance variable to store which
+        squares the Queen can attack at that position.
+        """
+
+        for square in range(64):
+            
+            # Get diagonal, perpendicular blockers
+            bMask = self.maskBlockers(square, self.bDIRECTIONS)
+            rMask = self.maskBlockers(square, self.rDIRECTIONS)
+
+            # Get permutations of blockers
+            bPermutations = self.permuteBlockers(bMask)
+            rPermutations = self.permuteBlockers(rMask)
+
+            bMagic = magicmoves.bishopMagic[square]
+            bShift = magicmoves.bishopShift[square]
+            rMagic = magicmoves.rookMagic[square]
+            rShift = magicmoves.rookShift[square]
+
+            for blocker in bPermutations:
+                index = ((blocker & bMask) * bMagic) >> bShift
+                squares = self.maskSlidingAttacks(square, self.bDIRECTIONS, blocker)
+                self.attacks[square][index] = squares
+
+            for blocker in rPermutations:
+                index = ((blocker & rMask) * rMagic) >> rShift
+                squares = self.maskSlidingAttacks(square, self.rDIRECTIONS, blocker)
+                
+                if (index in self.attacks[square]):
+                    self.attacks[square][index] |= squares
+                else: self.attacks[square][index] = squares
+
+
+
+
