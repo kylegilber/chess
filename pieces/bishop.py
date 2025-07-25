e6e6aa3ce076d8c1c2351a44f57bd4ef89944dea
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-24 21:50:05 -0400

    Update bishop.py
    Define getAttacks for bishops to store the positions on the board a bishop can attack for every square and possible blocker.

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -1,12 +1,36 @@
 from pieces.piece import Piece
+import magicmoves
 
 class Bishop(Piece):
 
-    MOVES = [
+    DIRECTIONS = [
         (1, 1), (-1, 1), (1, -1), (-1, -1)
     ]
 
     def __init__(self):
-        super().__init__()
+        self.attacks = [{} for square in range(64)]
 
-    
+    def getAttacks(self):
+        """
+        Generate attack tables for bishops.
+
+        For each possible bishop position on the board,
+        update the attacks instance variable to store which
+        squares the bishop can attack at that position.
+        """
+
+        for square in range(64):
+
+            # Generate blockers
+            mask = self.maskBlockers(square, self.DIRECTIONS)
+
+            # Generate all blocker permutations
+            permutations = self.permuteBlockers(mask)
+
+            magicNum = magicmoves.bishopMagic[square]
+            shiftNum = magicmoves.bishopShift[square]
+
+            for blocker in permutations:
+                index = ((blocker & mask) * magicNum) >> shiftNum
+                attacks = self.maskSlidingAttacks(square, self.DIRECTIONS, blocker)
+                self.attacks[square][index] = attacks
