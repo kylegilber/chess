ecaa532ef51752baef57f85fa37ff9c6bc7dc97a
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2025-07-26 00:49:18 -0400

    Update bishop.py
    Define the getAttacks function to select the pertinent bitboard of attackable squares for a given Bishop from the attack tables.

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -16,19 +16,34 @@ class Bishop(Piece):
         """
 
         for square in range(64):
+
             permutations = self.bPermutations[square]
-            magicNum = magicmoves.bishopMagic[square]
-            shiftNum = magicmoves.bishopShift[square]
+            magic = magicmoves.bishopMagic[square]
+            shift = magicmoves.bishopShift[square]
 
             for blocker in permutations:
-                index = (blocker * magicNum) >> shiftNum
+                index = (blocker * magic) >> shift
                 squares = self.maskSlidingAttacks(square, self.bDirections, blocker)
                 self.attacks[square][index] = squares
 
-    def getAttacks(self, square, pos):
+    def getAttacks(self, square, blockers):
         """
+        Get pseudo-legal Bishop attacks.
+
+        {args}
+        square (int): index of square
+        blockers (uint64): bitboard of blockers
+
+        {returns}
+        Bitboard (uint64) of attackable squares.
         """
 
+        magic = magicmoves.bishopMagic[square]
+        shift = magicmoves.bishopShift[square]
+        index = (blockers * magic) >> shift
+
+        return self.attacks[square][index]
+     
     def getMoves(self, square, pieces, allies):
         """
         """
